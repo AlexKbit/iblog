a4e2e310f83ff4d332d3bf94f163abbc8549c874
Aleksandr Savchenko <Aleksandr.Savchenko@infotecs.ru>
2017-01-12 18:31:45 +0300

    Update web configuration

--- app/src/main/java/com/alexkbit/iblog/app/config/WebConfiguration.java
+++ app/src/main/java/com/alexkbit/iblog/app/config/WebConfiguration.java
@@ -1,11 +1,13 @@
 package com.alexkbit.iblog.app.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
+@ComponentScan
 public class WebConfiguration extends WebMvcConfigurerAdapter {
 
 }
