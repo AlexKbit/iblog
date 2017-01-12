a77685df7b54e42ff562f0cb3cf4946155322ecd
AlexKbit <sav4enkoaleksandr@gmail.com>
2017-01-12 17:19:38 +0300

    Create web configuration

---
+++ app/src/main/java/com/alexkbit/iblog/app/config/WebConfiguration.java
@@ -0,0 +1,11 @@
+package com.alexkbit.iblog.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+
+}
