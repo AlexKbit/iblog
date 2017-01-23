9116f93334324e939185a6b157c003243410bb20
AlexKbit <sav4enkoaleksandr@gmail.com>
2017-01-23 16:43:46 +0300

    New method in user controller

--- rest/src/main/java/com/alexkbit/iblog/rest/api/UserController.java
+++ rest/src/main/java/com/alexkbit/iblog/rest/api/UserController.java
@@ -7,6 +7,7 @@ import com.alexkbit.iblog.servvices.api.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,11 +23,21 @@ public class UserController extends RESTController<User, UserDTO> {
     private UserService userService;
 
     @RequestMapping(method = RequestMethod.GET, value = "/{id}")
-    ResponseEntity<?> getUser(@PathVariable String id) {
+    ResponseEntity<?> get(@PathVariable String id) {
         User user = userService.get(id);
         if (user == null) {
             return ResponseEntity.noContent().build();
         }
+        return ResponseEntity.ok(mapToDTO(user));
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/")
+    ResponseEntity<?> register(@RequestBody UserDTO userDTO) {
+        User user = mapToModel(userDTO);
+        if (user == null) {
+            return ResponseEntity.noContent().build();
+        }
+        user = userService.register(user);
         return ResponseEntity.ok(mapToDTO(user));
     }
 }
