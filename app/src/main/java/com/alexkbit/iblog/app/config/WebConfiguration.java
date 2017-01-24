6b5354c6859d79c238d8816e3447940457800ec2
AlexKbit <sav4enkoaleksandr@gmail.com>
2017-01-24 12:04:23 +0300

    Update spring boot version to 1.4.3

--- app/src/main/java/com/alexkbit/iblog/app/config/WebConfiguration.java
+++ app/src/main/java/com/alexkbit/iblog/app/config/WebConfiguration.java
@@ -3,6 +3,7 @@ package com.alexkbit.iblog.app.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -10,4 +11,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @ComponentScan
 public class WebConfiguration extends WebMvcConfigurerAdapter {
 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+    }
 }
