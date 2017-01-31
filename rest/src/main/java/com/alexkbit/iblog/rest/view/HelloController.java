713c43cc1a422e4ffd20b5eae6caba6bc06b5537
AlexKbit <sav4enkoaleksandr@gmail.com>
2017-01-31 12:33:41 +0300

    Update web configurations

--- rest/src/main/java/com/alexkbit/iblog/rest/view/HelloController.java
+++ rest/src/main/java/com/alexkbit/iblog/rest/view/HelloController.java
@@ -1,16 +1,19 @@
 package com.alexkbit.iblog.rest.view;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Sample REST Controller
  */
-@RestController
+@Controller
 public class HelloController {
 
     @RequestMapping("/hi")
-    public String hello() {
-        return "Hi user!";
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
+        return "hello";
     }
 }
