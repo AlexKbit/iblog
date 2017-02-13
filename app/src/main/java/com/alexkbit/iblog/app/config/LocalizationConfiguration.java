61c4a22c8aeeb33af5cef9c1662515be36f3717a
AlexKbit <sav4enkoaleksandr@gmail.com>
2017-02-13 15:12:14 +0300

    Add resource bundle

---
+++ app/src/main/java/com/alexkbit/iblog/app/config/LocalizationConfiguration.java
@@ -0,0 +1,30 @@
+package com.alexkbit.iblog.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+/**
+ * Config for locale in app
+ */
+@Configuration
+public class LocalizationConfiguration {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+}
