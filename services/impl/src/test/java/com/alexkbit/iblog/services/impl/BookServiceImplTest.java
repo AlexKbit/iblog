96399dc10e969fca2f723d4e4b817259f2344f3e
AlexKbit <sav4enkoaleksandr@gmail.com>
2017-03-29 23:16:58 +0300

    Refactoring

--- services/impl/src/test/java/com/alexkbit/iblog/services/impl/BookServiceImplTest.java
+++ services/impl/src/test/java/com/alexkbit/iblog/services/impl/BookServiceImplTest.java
@@ -17,7 +17,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 /**
- * Test for {@link ImageServiceImpl}
+ * Test for {@link BookServiceImpl}
  */
 @RunWith(EasyMockRunner.class)
 public class BookServiceImplTest extends EasyMockSupport {
