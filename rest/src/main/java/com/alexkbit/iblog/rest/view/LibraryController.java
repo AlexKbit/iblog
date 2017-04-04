408bbfd61d5d659ff9a8448eac7fa6a2cd6663fd
AlexKbit <sav4enkoaleksandr@gmail.com>
2017-04-04 21:58:51 +0300

    Update page of library

--- rest/src/main/java/com/alexkbit/iblog/rest/view/LibraryController.java
+++ rest/src/main/java/com/alexkbit/iblog/rest/view/LibraryController.java
@@ -1,18 +1,24 @@
 package com.alexkbit.iblog.rest.view;
 
+import com.alexkbit.iblog.servvices.api.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
- * Created by Savchenko.Ale on 31.01.2017.
+ * Controller of library page
  */
 @Controller
 public class LibraryController {
 
-    @RequestMapping(value="/library", method = RequestMethod.GET)
-    public String index(Model model) {
-        return "library";
+    @Autowired
+    private BookService bookService;
+
+    @RequestMapping("/library")
+    public ModelAndView get(@RequestParam(value = "page", defaultValue = "0") Integer page,
+                            @RequestParam(value = "count", defaultValue = "10") Integer count) {
+        return new ModelAndView("library", "page", bookService.get(page, count));
     }
 }
--- web/src/main/resources/templates/library.ftl
+++ web/src/main/resources/templates/library.ftl
@@ -1,5 +1,24 @@
 <#import "layout.ftl" as layout>
+<#import "/spring.ftl" as spring/>
 
 <@layout.page pageTitle="">
-<h3>Library</h3>
+<div>
+    <div class="text-center">
+        <h3><@spring.message "messages.library"/></h3>
+    </div>
+    <div>
+        <#list page.content as book>
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <#if book.image?? && book.image.id??>
+                        <img src="/image/${book.image.id}" class="image-book-small">
+                    <#else>
+                        <img src="/images/noImage.png" class="image-book-small">
+                    </#if>
+                </div>
+            </div>
+        </#list>
+        <#include "pagination.ftl">
+    </div>
+</div>
 </@layout.page>
