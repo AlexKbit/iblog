dd3101a0989ccd46e5c4181d71a4d804e85a3c46
AlexKbit <sav4enkoaleksandr@gmail.com>
2017-09-19 23:16:11 +0300

    Update dto for post

--- rest/src/main/java/com/alexkbit/iblog/rest/dto/PostDTO.java
+++ rest/src/main/java/com/alexkbit/iblog/rest/dto/PostDTO.java
@@ -17,4 +17,35 @@ public class PostDTO extends BaseDTO {
 
     private List<PostElementDTO> elements;
 
+    public String getTheme() {
+        return theme;
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+
+    public Image getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(Image imageId) {
+        this.imageId = imageId;
+    }
+
+    public List<TechnologyDTO> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<TechnologyDTO> tags) {
+        this.tags = tags;
+    }
+
+    public List<PostElementDTO> getElements() {
+        return elements;
+    }
+
+    public void setElements(List<PostElementDTO> elements) {
+        this.elements = elements;
+    }
 }
